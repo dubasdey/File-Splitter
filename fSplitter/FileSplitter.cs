d556a7c3929b0505512a9e65fd4f7e2569e3b8ce
dubasdey <dubasdey@43c676d2-2e83-41cc-8d7b-37e7d77d3ff0>
2009-10-28 15:31:37 +0000

    First Release

---
+++ fSplitter/FileSplitter.cs
@@ -0,0 +1,335 @@
+﻿/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace FileSplitter {
+
+    /// <summary>
+    /// Arguments for Split process event
+    /// </summary>
+    internal class SplitProcessArgs: EventArgs{
+       
+        /// <summary>
+        /// File Name
+        /// </summary>
+        private String fileName  = String.Empty;
+
+        /// <summary>
+        /// Part number of total parts
+        /// </summary>
+        private double part = 0;
+
+        /// <summary>
+        /// Partsize Written
+        /// </summary>
+        private double partSizeWritted = 0;
+
+        /// <summary>
+        /// Total parts
+        /// </summary>
+        private double parts;
+
+        /// <summary>
+        /// Size in bytes of each part
+        /// </summary>
+        private double partSize;
+
+        /// <summary>
+        /// Geter for FileName
+        /// </summary>
+        public String FileName {
+            get { return fileName; }
+        }
+
+        /// <summary>
+        /// Getter for Actual Part
+        /// </summary>
+        public double Part {
+            get { return part; }
+        }
+
+        /// <summary>
+        /// Getter for bytes written of actual Part
+        /// </summary>
+        public double PartSizeWritted {
+            get { return partSizeWritted; }
+        }
+
+        /// <summary>
+        /// Getter for  Size of a part in bytes
+        /// </summary>
+        public double PartSize {
+            get { return partSize; }
+            set { partSize = value; }
+        }
+
+        /// <summary>
+        /// Getter for  Total parts expected
+        /// </summary>
+        public double Parts {
+            get { return parts; }
+            set { parts = value; }
+        }
+
+        /// <summary>
+        /// Argument constructor
+        /// </summary>
+        /// <param name="file">Actual processing file</param>
+        /// <param name="part">Actual part</param>
+        /// <param name="partSizeWritten">Files written in this part</param>
+        /// <param name="totalParts">Total parts</param>
+        /// <param name="partSize">Total size expected of each part</param>
+        public SplitProcessArgs(String file, double part, double partSizeWritten,double totalParts,double partSize) {
+            this.fileName = file;
+            this.part  = part;
+            this.partSizeWritted = partSizeWritten;
+            this.parts = totalParts;
+            this.partSize = partSize;
+        }
+    }
+
+
+    /// <summary>
+    /// File Splitter class.
+    /// This class do all calc and splitting operations
+    /// </summary>
+    internal class FileSplitter {
+
+        /// <summary>
+        /// Default buffer size
+        /// </summary>
+        private static Int32 BUFFER_SIZE = 1024 * 4;
+        private static Int32 BUFFER_SIZE_BIG = 1024 * 1024 * 10;
+       
+
+        /// <summary>
+        /// Delegate for Split star
+        /// </summary>
+        public delegate void splitStartHandler();
+
+        /// <summary>
+        /// Delegate for Split end
+        /// </summary>
+        public delegate void splitEndHandler();
+
+        /// <summary>
+        /// Delegate for Split process
+        /// </summary>
+        /// <param name="sender">splitter</param>
+        /// <param name="args">process parameters</param>
+        public delegate void splitProcessHandler(Object sender,SplitProcessArgs args);
+
+        /// <summary>
+        /// Spliter Start event
+        /// </summary>
+        public event splitStartHandler splitStart;
+
+        /// <summary>
+        /// Splitern End event
+        /// </summary>
+        public event splitEndHandler splitEnd;
+
+        /// <summary>
+        /// Splitter process event
+        /// </summary>
+        public event splitProcessHandler splitProcess;
+
+
+        /// <summary>
+        /// Size of a part in bytes
+        /// </summary>
+        private double partSizeBytes;
+
+        /// <summary>
+        /// Source filename to split
+        /// </summary>
+        private String fileName;
+
+        /// <summary>
+        /// Splitter constructor
+        /// </summary>
+        public FileSplitter(){
+            partSizeBytes = 0;
+            fileName = String.Empty;
+        }
+
+        /// <summary>
+        /// Getter for the part size in bytes
+        /// </summary>
+        public double PartSize {
+            get { return partSizeBytes; }
+        }
+        
+        /// <summary>
+        /// Sets the part size
+        /// </summary>
+        /// <param name="partSize">part size in partSizeUnits scale</param>
+        /// <param name="partSizeUnits">
+        ///     Unit of partsize
+        ///     <list type="">
+        ///         <item>0 bytes</item>
+        ///         <item>1 Kbytes</item>
+        ///         <item>2 Mbytes</item>
+        ///         <item>3 Gbytes</item>
+        ///     </list>
+        /// </param>
+        public void setPartSize(decimal partSize, Int32 partSizeUnits) {
+            Int32 index = partSizeUnits;
+            partSizeBytes = Convert.ToDouble(partSize);
+            if (index > 0) {
+                partSizeBytes = Convert.ToInt32(partSize) * Math.Pow(1024, index);
+            }
+        }
+
+        /// <summary>
+        /// Filename to be splitted
+        /// </summary>
+        public String FileName {
+            get { return fileName; }
+            set { fileName = value; }
+        }
+
+        /// <summary>
+        /// Calculates number of parts, based on size of file a part size
+        /// </summary>
+        public double Parts {
+            get {
+                double parts = 1;
+                if (fileName.Length > 0 && File.Exists(fileName)) {
+                    FileInfo fi = new FileInfo(fileName);
+                    if (fi.Length > partSizeBytes) {
+                        parts = Math.Ceiling((double)fi.Length / partSizeBytes);
+                    }
+                }
+                return parts;
+            }
+        }
+
+        /// <summary>
+        /// Launch splitStart event
+        /// </summary>
+        private void onSplitStart() {
+            if (splitStart != null) {
+                splitStart();
+            }
+        }
+
+        /// <summary>
+        /// Launch splitEnd event
+        /// </summary>
+        private void onSplitEnd() {
+            if (splitEnd != null) {
+                splitEnd();
+            }
+        }
+
+        /// <summary>
+        /// Launch splitProcess event
+        /// </summary>
+        /// <param name="filename">actual processing filename</param>
+        /// <param name="part">actual part</param>
+        /// <param name="partSizeWritten">bytes written in this part</param>
+        /// <param name="totalParts">total parts</param>
+        /// <param name="partSize">part size</param>
+        private void onSplitProcess(String filename, double part, double partSizeWritten, double totalParts, double partSize) {
+            if (splitProcess != null) {
+                splitProcess(this, new SplitProcessArgs(filename, part, partSizeWritten, totalParts, partSize));
+            }
+        }
+
+        /// <summary>
+        /// Do split operation
+        /// </summary>
+        public void doSplit() {
+            onSplitStart();
+
+            try {
+                FileInfo fileNameInfo = new FileInfo (fileName);
+                DriveInfo driveInfo = new DriveInfo(fileName);
+                if (driveInfo.AvailableFreeSpace <= fileNameInfo.Length) {
+                    MessageBox.Show("No Space available to split", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    onSplitEnd();
+                    return;
+                }
+
+                FileStream stmOriginal = null;
+                try {
+                    stmOriginal = File.OpenRead(fileName);
+                } catch {
+                    MessageBox.Show("Error opening " + fileName, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    onSplitEnd();
+                    return;
+                }
+
+                byte[] buffer = new byte[BUFFER_SIZE];
+                // if buffer is greater than partsize less bufer size
+                if (partSizeBytes < BUFFER_SIZE) {
+                    buffer = new byte[(int)partSizeBytes];
+                    // if partSizeBytes is greater than 10 mb buffer of 10mb
+                    // this reduces read/writting operations
+                } else if (partSizeBytes > BUFFER_SIZE_BIG) {
+                    buffer = new byte[BUFFER_SIZE_BIG];
+                }
+
+                double totalParts = this.Parts;
+                int readedBytes = stmOriginal.Read(buffer, 0, buffer.Length);
+                double actualPartSizeWritted = 0;
+                double actualFileNumber = 0;
+                String actualFileName = fileName + "." + actualFileNumber;
+                FileStream stmWritter = File.Create(actualFileName);
+
+
+
+                while (readedBytes > 0) {
+
+                    // write buffer
+                    stmWritter.Write(buffer, 0, readedBytes);
+
+                    // increase part size
+                    actualPartSizeWritted += readedBytes;
+
+                    // launch processing event
+                    onSplitProcess(actualFileName, actualFileNumber, actualPartSizeWritted, totalParts, partSizeBytes);
+
+                    // nextPart?
+                    if (actualPartSizeWritted >= partSizeBytes) {
+                        actualFileNumber++;
+                        actualPartSizeWritted = 0;
+                        stmWritter.Flush();
+                        stmWritter.Close();
+                        actualFileName = fileName + "." + actualFileNumber;
+                        stmWritter = File.Create(actualFileName);
+                    }
+
+                    // if readed less than buffer is last part
+                    if (readedBytes < buffer.Length) {
+                        break;
+                    }
+                    // read next
+                    readedBytes = stmOriginal.Read(buffer, 0, buffer.Length);
+                }
+            } catch {
+                MessageBox.Show("Error creating files", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } finally {
+                onSplitEnd();
+            }
+        }
+    }
+}
---
+++ fSplitter/FrmSplitter.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace FileSplitter {
+    partial class FrmSplitter {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.lbFile = new System.Windows.Forms.Label();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.cmdSelectFile = new System.Windows.Forms.Button();
+            this.grpSplitSize = new System.Windows.Forms.GroupBox();
+            this.lbEstimatedParts = new System.Windows.Forms.Label();
+            this.cmbUnits = new System.Windows.Forms.ComboBox();
+            this.numSize = new System.Windows.Forms.NumericUpDown();
+            this.cmdStart = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.panelSpitting = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBarFileSize = new System.Windows.Forms.ProgressBar();
+            this.progressBarFiles = new System.Windows.Forms.ProgressBar();
+            this.lbSplitInfo = new System.Windows.Forms.Label();
+            this.lbInfo = new System.Windows.Forms.Label();
+            this.grpSplitSize.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numSize)).BeginInit();
+            this.panelSpitting.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbFile
+            // 
+            this.lbFile.AutoSize = true;
+            this.lbFile.Location = new System.Drawing.Point(12, 9);
+            this.lbFile.Name = "lbFile";
+            this.lbFile.Size = new System.Drawing.Size(23, 13);
+            this.lbFile.TabIndex = 0;
+            this.lbFile.Text = "File";
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(41, 6);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.ReadOnly = true;
+            this.txtFile.Size = new System.Drawing.Size(241, 20);
+            this.txtFile.TabIndex = 1;
+            // 
+            // cmdSelectFile
+            // 
+            this.cmdSelectFile.Location = new System.Drawing.Point(293, 4);
+            this.cmdSelectFile.Name = "cmdSelectFile";
+            this.cmdSelectFile.Size = new System.Drawing.Size(37, 23);
+            this.cmdSelectFile.TabIndex = 2;
+            this.cmdSelectFile.Text = "...";
+            this.cmdSelectFile.UseVisualStyleBackColor = true;
+            this.cmdSelectFile.Click += new System.EventHandler(this.cmdSelectFile_Click);
+            // 
+            // grpSplitSize
+            // 
+            this.grpSplitSize.Controls.Add(this.lbEstimatedParts);
+            this.grpSplitSize.Controls.Add(this.cmbUnits);
+            this.grpSplitSize.Controls.Add(this.numSize);
+            this.grpSplitSize.Location = new System.Drawing.Point(15, 32);
+            this.grpSplitSize.Name = "grpSplitSize";
+            this.grpSplitSize.Size = new System.Drawing.Size(246, 46);
+            this.grpSplitSize.TabIndex = 3;
+            this.grpSplitSize.TabStop = false;
+            this.grpSplitSize.Text = "Split Size";
+            // 
+            // lbEstimatedParts
+            // 
+            this.lbEstimatedParts.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEstimatedParts.Location = new System.Drawing.Point(6, 16);
+            this.lbEstimatedParts.Name = "lbEstimatedParts";
+            this.lbEstimatedParts.Size = new System.Drawing.Size(104, 20);
+            this.lbEstimatedParts.TabIndex = 2;
+            this.lbEstimatedParts.Tag = "{0} parts of";
+            this.lbEstimatedParts.Text = "0 parts of";
+            this.lbEstimatedParts.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // cmbUnits
+            // 
+            this.cmbUnits.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbUnits.FormattingEnabled = true;
+            this.cmbUnits.Items.AddRange(new object[] {
+            "b",
+            "Kb",
+            "Mb"});
+            this.cmbUnits.Location = new System.Drawing.Point(184, 16);
+            this.cmbUnits.Name = "cmbUnits";
+            this.cmbUnits.Size = new System.Drawing.Size(56, 21);
+            this.cmbUnits.TabIndex = 1;
+            this.cmbUnits.SelectedIndexChanged += new System.EventHandler(this.controlValueChangedEvent);
+            // 
+            // numSize
+            // 
+            this.numSize.Location = new System.Drawing.Point(116, 16);
+            this.numSize.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.numSize.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numSize.Name = "numSize";
+            this.numSize.Size = new System.Drawing.Size(62, 20);
+            this.numSize.TabIndex = 0;
+            this.numSize.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numSize.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numSize.ValueChanged += new System.EventHandler(this.controlValueChangedEvent);
+            // 
+            // cmdStart
+            // 
+            this.cmdStart.Enabled = false;
+            this.cmdStart.Location = new System.Drawing.Point(267, 40);
+            this.cmdStart.Name = "cmdStart";
+            this.cmdStart.Size = new System.Drawing.Size(63, 33);
+            this.cmdStart.TabIndex = 4;
+            this.cmdStart.Text = "Start";
+            this.cmdStart.UseVisualStyleBackColor = true;
+            this.cmdStart.Click += new System.EventHandler(this.lbStart_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.DefaultExt = "log";
+            this.openFileDialog.Filter = "All Files|*.*";
+            // 
+            // panelSpitting
+            // 
+            this.panelSpitting.Controls.Add(this.label2);
+            this.panelSpitting.Controls.Add(this.label1);
+            this.panelSpitting.Controls.Add(this.progressBarFileSize);
+            this.panelSpitting.Controls.Add(this.progressBarFiles);
+            this.panelSpitting.Controls.Add(this.lbSplitInfo);
+            this.panelSpitting.Location = new System.Drawing.Point(15, 84);
+            this.panelSpitting.Name = "panelSpitting";
+            this.panelSpitting.Size = new System.Drawing.Size(315, 80);
+            this.panelSpitting.TabIndex = 5;
+            this.panelSpitting.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Actual file progress";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "All files progress";
+            // 
+            // progressBarFileSize
+            // 
+            this.progressBarFileSize.BackColor = System.Drawing.Color.White;
+            this.progressBarFileSize.ForeColor = System.Drawing.Color.Green;
+            this.progressBarFileSize.Location = new System.Drawing.Point(3, 63);
+            this.progressBarFileSize.Name = "progressBarFileSize";
+            this.progressBarFileSize.Size = new System.Drawing.Size(307, 12);
+            this.progressBarFileSize.Step = 1;
+            this.progressBarFileSize.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBarFileSize.TabIndex = 2;
+            this.progressBarFileSize.UseWaitCursor = true;
+            // 
+            // progressBarFiles
+            // 
+            this.progressBarFiles.BackColor = System.Drawing.Color.White;
+            this.progressBarFiles.ForeColor = System.Drawing.Color.Green;
+            this.progressBarFiles.Location = new System.Drawing.Point(3, 32);
+            this.progressBarFiles.Name = "progressBarFiles";
+            this.progressBarFiles.Size = new System.Drawing.Size(307, 12);
+            this.progressBarFiles.Step = 1;
+            this.progressBarFiles.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBarFiles.TabIndex = 1;
+            this.progressBarFiles.UseWaitCursor = true;
+            // 
+            // lbSplitInfo
+            // 
+            this.lbSplitInfo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbSplitInfo.Location = new System.Drawing.Point(0, 0);
+            this.lbSplitInfo.Name = "lbSplitInfo";
+            this.lbSplitInfo.Size = new System.Drawing.Size(315, 14);
+            this.lbSplitInfo.TabIndex = 0;
+            this.lbSplitInfo.Tag = "Splitting file {0}";
+            this.lbSplitInfo.Text = "Splitting file {0}";
+            // 
+            // lbInfo
+            // 
+            this.lbInfo.Location = new System.Drawing.Point(13, 84);
+            this.lbInfo.Name = "lbInfo";
+            this.lbInfo.Size = new System.Drawing.Size(318, 80);
+            this.lbInfo.TabIndex = 7;
+            this.lbInfo.Text = "First select a file to split. Next select the part size and  push on \"Start\" to s" +
+                "tart splitting the file in smaller parts";
+            this.lbInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FrmSplitter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 169);
+            this.Controls.Add(this.panelSpitting);
+            this.Controls.Add(this.cmdStart);
+            this.Controls.Add(this.grpSplitSize);
+            this.Controls.Add(this.cmdSelectFile);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.lbFile);
+            this.Controls.Add(this.lbInfo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmSplitter";
+            this.Text = "File Splitter";
+            this.grpSplitSize.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numSize)).EndInit();
+            this.panelSpitting.ResumeLayout(false);
+            this.panelSpitting.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbFile;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button cmdSelectFile;
+        private System.Windows.Forms.GroupBox grpSplitSize;
+        private System.Windows.Forms.ComboBox cmbUnits;
+        private System.Windows.Forms.NumericUpDown numSize;
+        private System.Windows.Forms.Label lbEstimatedParts;
+        private System.Windows.Forms.Button cmdStart;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Panel panelSpitting;
+        private System.Windows.Forms.Label lbSplitInfo;
+        private System.Windows.Forms.ProgressBar progressBarFiles;
+        private System.Windows.Forms.ProgressBar progressBarFileSize;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbInfo;
+    }
+}
+
