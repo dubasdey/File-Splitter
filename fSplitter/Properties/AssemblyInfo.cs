695d9c74ba5a44c245de7bca8dcb7fc07f6101dc
dubasdey <dubasdey@43c676d2-2e83-41cc-8d7b-37e7d77d3ff0>
2011-12-04 12:43:34 +0000

    Fixed bug #3450220

--- fSplitter/Properties/AssemblyInfo.cs
+++ fSplitter/Properties/AssemblyInfo.cs
@@ -31,6 +31,5 @@ using System.Runtime.InteropServices;
 //
 // Puede especificar todos los valores o establecer como predeterminados los números de versión de compilación y de revisión 
 // mediante el asterisco ('*'), como se muestra a continuación:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: AssemblyFileVersion("1.2.1.0")]
+[assembly: AssemblyVersion("1.2.3.*")]
+
