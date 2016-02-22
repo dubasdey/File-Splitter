176027d27321b8c2a04a35da16f190d6addbf1fe
erodriguez <eduardo@rodriguezcarro.es>
2016-02-22 21:40:31 +0100

    Added 28Kb file to 4kb parts tests

--- Test/test.bat
+++ Test/test.bat
@@ -1,3 +1,8 @@
 md result
-copy test_256.txt toDelete.txt
-..\bin\Debug\fSplit.exe -split 1 kb toDelete.txt -d -f sp_{0:0000}_of_{1:0000}.txt -df result -lf files.txt
+echo TEST1
+REM copy test_256.txt toDelete_256.txt
+REM ..\bin\Debug\fSplit.exe -split 1 kb toDelete_256.txt -d -f sp_{0:0000}_of_{1:0000}.txt -df result -lf files.txt
+
+echo TEST2
+copy test_28.txt toDelete28.txt 
+..\bin\Debug\fSplit.exe -split 4 kb toDelete28.txt -df result
